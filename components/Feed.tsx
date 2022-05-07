aee4c7d184ad92810044062821e240880f64932e
mariosknl <mariosknl@outlook.com>
2022-05-07 18:27:34 +0300

    building the tweetbox component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,6 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import TweetBox from './TweetBox'
 
 function Feed() {
   return (
@@ -10,6 +11,9 @@ function Feed() {
       </div>
 
       {/* Tweetbox */}
+      <div>
+        <TweetBox />
+      </div>
     </div>
   )
 }
---
+++ components/TweetBox.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from '@heroicons/react/outline'
+
+function TweetBox() {
+  const [input, setInput] = useState<string>('')
+
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        src="https://pbs.twimg.com/profile_images/1250077050662240256/RH_l8Z3s_400x400.jpg"
+        alt="profile image"
+        className="mr-5 mt-4 h-14 w-14 rounded-full object-cover"
+      />
+
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            value={input}
+            type="text"
+            placeholder="What's happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
+            </div>
+
+            <button
+              disabled={!input}
+              className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default TweetBox
