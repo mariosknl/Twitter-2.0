960ca8a677a5e20de476f168cf8c921f24ee1ac7
mariosknl <mariosknl@outlook.com>
2022-05-07 17:34:00 +0300

    building the sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
+
+function Sidebar() {
+  return (
+    <div>
+      <img
+        className="h-10 w-10"
+        src="https://links.papareact.com/drq"
+        alt="twitter logo"
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
