91ab13fc1da2dc6df94e52f3f4bca1eb8b84daf5
mariosknl <mariosknl@outlook.com>
2022-05-08 12:37:28 +0300

    implementing comment functionality part2

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -25,8 +25,6 @@ function Tweet({ tweet }: Props) {
     refreshComments()
   }, [])
 
-  console.log(comments)
-
   return (
     <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
       <div className="flex space-x-3">
@@ -64,7 +62,7 @@ function Tweet({ tweet }: Props) {
       <div className="mt-5 flex justify-between">
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <ChatAlt2Icon className="h-5 w-5" />
-          <p>5</p>
+          <p>{comments.length}</p>
         </div>
 
         <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
@@ -79,6 +77,34 @@ function Tweet({ tweet }: Props) {
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
+
+      {comments?.length > 0 && (
+        <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
+          {comments.map((comment) => (
+            <div key={comment._id} className="relative flex space-x-2">
+              <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+              <img
+                src={comment.profileImg}
+                alt=""
+                className="mt-2 h-7 w-7 rounded-full object-cover"
+              />
+              <div>
+                <div className="flex items-center space-x-1">
+                  <p className="mr-1 font-bold">{comment.username}</p>
+                  <p className="hidden text-sm text-gray-500 lg:inline">
+                    @{tweet.username.replace(/\s+/g, '').toLowerCase()} •
+                  </p>
+                  <TimeAgo
+                    className="text-sm text-gray-500"
+                    date={comment._createdAt}
+                  />
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
